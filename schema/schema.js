529857f81c971c6abf787cd85831d71a71bf39fc
dickyboKa <dicky.kamar14@gmail.com>
2019-05-30 11:31:23 +0700

    Something wrong with the database will try to figure it out

--- schema/schema.js
+++ schema/schema.js
@@ -7,13 +7,27 @@ const {
   GraphQLSchema
 } = graphql;
 
+const CompanyType = new GraphQLObjectType({
+    name: 'Company',
+    fields: {
+      id: { type: GraphQLString},
+      name: { type: GraphQLString},
+      description: { type: GraphQLSchema}
+    }
+});
 
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: {
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
-    age: { type: GraphQLInt }
+    age: { type: GraphQLInt },
+    company: {
+      type: CompanyType,
+      resolve(parentValue, args) {
+        console.log(parentValue, args);
+      }
+    }
   }
 });
 
